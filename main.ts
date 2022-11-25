108f893d4fd58a0050f36bbf7033de6599c549d4
SamsonBox <samson.box@gmx.net>
2022-11-25 20:17:54 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
+let zahl = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    for (let index = 0; index < 1; index++) {
+    for (let index = 0; index < 2; index++) {
         basic.showString("hallo paul")
         basic.pause(500)
     }
-    music.playMelody("- - - - - - - - ", 136)
+    music.playMelody("C5 B A G F E D C ", 136)
     basic.showString("hi!")
     basic.showLeds(`
         # # # # #
@@ -14,5 +15,101 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
         `)
     basic.showNumber(5000)
     basic.showString("hallo papa  hallo mama  hallo emil")
-    music.playMelody("- - - - - - - - ", 136)
+    music.playMelody("C5 A B G A F G E ", 136)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    input.calibrateCompass()
+    basic.showIcon(IconNames.Yes)
+    basic.showLeds(`
+        # # . # .
+        # # # # .
+        . # # # #
+        # # # # #
+        . . # # #
+        `)
+    basic.showString("was ist die gefärlichste sport art der welt")
+})
+input.onGesture(Gesture.Shake, function () {
+    zahl = randint(1, 6)
+    if (zahl == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (zahl == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . # # . .
+            # . . # .
+            . . # . .
+            . # . . .
+            # # # # .
+            `)
+    } else if (zahl == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            # # # . .
+            . . # . .
+            . # . . .
+            . . # . .
+            # # . . .
+            `)
+    } else if (zahl == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            # . . . .
+            # . # . .
+            # # # . .
+            . . # . .
+            . . # . .
+            `)
+    } else if (zahl == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . # . #
+            `)
+    }
 })
